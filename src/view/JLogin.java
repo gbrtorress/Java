36139648386df6ca611ed51c9e51ea1f9678351e
Gabriel Torres <107062669+gbrtorress@users.noreply.github.com>
2023-09-11 11:03:17 -0300

    Criando tela de login

---
+++ src/view/JLogin.java
@@ -0,0 +1,103 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class JLogin extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textFieldUsuario;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JLogin frame = new JLogin();
+					frame.setLocationRelativeTo(null);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JLogin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 514, 306);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(49, 62, 64));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(123, 11, 277, 227);
+		panel.setBackground(new Color(204, 207, 208));
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Usuário");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel.setBounds(34, 46, 67, 14);
+		panel.add(lblNewLabel);
+		
+		textFieldUsuario = new JTextField();
+		textFieldUsuario.setBounds(34, 71, 160, 20);
+		panel.add(textFieldUsuario);
+		textFieldUsuario.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Senha");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_1.setBounds(34, 102, 74, 14);
+		panel.add(lblNewLabel_1);
+		
+		JButton btnNewButton = new JButton("Logar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (textFieldUsuario.getText() != null && 
+						!textFieldUsuario.getText().isEmpty() && 
+						passwordField.getText() != null && 
+						!passwordField.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(btnNewButton, "Informações válidas");
+					
+				}else {
+					JOptionPane.showMessageDialog(btnNewButton, "Verifique as informações", "Aviso", JOptionPane.WARNING_MESSAGE);
+				}
+			}
+		});
+		btnNewButton.setBackground(new Color(49, 62, 64));
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setBounds(84, 171, 81, 23);
+		panel.add(btnNewButton);
+		
+		JLabel lblNewLabel_2 = new JLabel("Bem Vindo");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_2.setBounds(93, 21, 101, 14);
+		panel.add(lblNewLabel_2);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(34, 127, 160, 20);
+		panel.add(passwordField);
+	}
+}
